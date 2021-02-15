49461b3e93209b5d3e9b53fa9df81acac869fa18
Martin Storsjö <martin@martin.st>
2021-02-15 11:17:17 +0200

    Remove vctip.exe on installation
    This should fix issue #23.

--- install.sh
+++ install.sh
@@ -40,6 +40,13 @@ for arch in x86 x64 arm arm64; do
     done
     cd ..
 done
+cd ../bin
+# vstip.exe is known to cause problems at some times; just remove it.
+# See https://bugs.chromium.org/p/chromium/issues/detail?id=735226 and
+# https://github.com/mstorsjo/msvc-wine/issues/23 for references.
+for i in $(find . -iname vctip.exe); do
+    rm $i
+done
 cd "$DEST"
 
 if [ -d kits/10 ]; then
