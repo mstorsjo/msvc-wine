12c6aede33beb8d2866fb7bcf03e9e87fe69a6ad
huangqinjin <huangqinjin@gmail.com>
2023-04-15 13:27:00 +0800

    Translate exit code of mt.exe in bat file

--- wrappers/wine-msvc.bat
+++ wrappers/wine-msvc.bat
@@ -1,3 +1,8 @@
 @echo off
 
 %* %WINE_MSVC_ARGS% >%WINE_MSVC_STDOUT% 2>%WINE_MSVC_STDERR%
+
+REM https://gitlab.kitware.com/cmake/cmake/-/blob/0991023c30ed5b83bcb1446b5bcc9c1eae028835/Source/cmcmd.cxx#L2388
+if /I "%~n1"=="mt" (
+    if %errorlevel%==1090650113 (exit 187)
+)
