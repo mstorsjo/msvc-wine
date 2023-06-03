5701901f2f8514dbde49b8939bfc61edb3a8122e
huangqinjin <huangqinjin@gmail.com>
2023-06-04 01:10:13 +0800

    Unixify paths for dumpbin /PDBPATH also

--- test/test.sh
+++ test/test.sh
@@ -91,6 +91,7 @@ for arch in x86 x64 arm arm64; do
     EXEC "" BIN=$BIN ./test-cl.sh
     EXEC "" BIN=$BIN ./test-mt.sh
     EXEC "" BIN=$BIN ./test-cmake.sh
+    EXEC "" BIN=$BIN ./test-dumpbin.sh
 done
 
 EXIT
