82d1e6c69cb0820c42e56f0d56681840840cecf1
Mark Harmstone <mark@harmstone.com>
2024-04-15 22:39:51 +0100

    Add wrapper for mc.exe

---
+++ test/test-mc.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#
+# Copyright (c) 2024 Mark Harmstone
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+. "${0%/*}/test.sh"
+
+
+cat >test-mc.mc <<EOF
+SeverityNames=(
+    Success=0x0:STATUS_SEVERITY_SUCCESS
+    Informational=0x1:STATUS_SEVERITY_INFORMATIONAL
+    Warning=0x2:STATUS_SEVERITY_WARNING
+    Error=0x3:STATUS_SEVERITY_ERROR
+)
+
+LanguageNames=(English=0x409:MSG00409)
+
+MessageIdTypedef=DWORD
+
+MessageId=0x100
+Severity=Informational
+SymbolicName=MSG_FOO
+Language=English
+foo
+.
+
+MessageId=0x101
+Severity=Warning
+SymbolicName=MSG_BAR
+Language=English
+bar
+.
+EOF
+
+EXEC "" ${BIN}mc test-mc.mc
+
+EXIT
