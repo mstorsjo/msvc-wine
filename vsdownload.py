7411cb83a85ff6e2b8d5383aa3a37113d4168672
huangqinjin <huangqinjin@gmail.com>
2026-02-28 18:22:18 +0800

    vsdownload: Allow `--msvc-version latest`

--- vsdownload.py
+++ vsdownload.py
@@ -202,7 +202,7 @@ def setPackageSelection(args, packages):
     # Note, that in the manifest for MSVC version X.Y, only version X.Y-1
     # exists with a package name like "Microsoft.VisualStudio.Component.VC."
     # + toolversion + ".x86.x64".
-    if args.msvc_version is None:
+    if args.msvc_version is None or args.msvc_version == "latest":
         args.package.extend(defaultPackages)
         args.ignore.extend(defaultIgnores)
     elif args.msvc_version == "preview":
